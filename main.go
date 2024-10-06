d4df0cfeaf61190f67f9fd36456a7ebab5e4192e
Evgeny <onepiece182tom@gmail.com>
2024-10-06 16:38:53 +0700

    добавлена функция обработки командной строки

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"strconv"
 )
 
 func getName(r io.Reader, w io.Writer) (string, error) {
@@ -20,6 +21,32 @@ func getName(r io.Reader, w io.Writer) (string, error) {
 		return "", errors.New("Ты не ввел свое имя")
 	}
 	return name, nil
+}
+
+type config struct {
+	numTimes   int
+	printUsage bool
+}
+
+func parceArg(args []string) (config, error) {
+	var numTimes int
+	var err error
+	c := config{}
+	if len(args) != 1 {
+		return c, errors.New("неверное количество аргументов ")
+	}
+
+	if args[0] == "-h" || args[0] == "- -help" {
+		c.printUsage = true
+		return c, nil
+	}
+
+	numTimes, err = strconv.Atoi(args[0])
+	if err != nil {
+		return c, err
+	}
+	c.numTimes = numTimes
+	return c, nil
 }
 
 func main() {
